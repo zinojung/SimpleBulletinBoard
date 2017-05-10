6c70a497af38dafdbac37055859622b7861954c3
ZINO\46fir <46firstmate@gmail.com>
2017-05-11 00:08:38 +0900

    새로운 템플릿에 회원가입기능 구현

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,27 +1,31 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+
 @Controller
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@PostMapping("/create")
+	@Autowired
+	private UserRepository userRepository;
+	@PostMapping("/user/create")
 	public String create(User user){
 		System.out.println("user : " + user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/user/list";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/user/list")
 	public String list(Model model){
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
 }
