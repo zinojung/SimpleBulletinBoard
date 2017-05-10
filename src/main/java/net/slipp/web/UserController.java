3a6e61b3942591a549ce7cde564d89e151f46427
ZINO\46fir <46firstmate@gmail.com>
2017-05-11 02:49:28 +0900

    include navigation and footer

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,31 +1,41 @@
 package net.slipp.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
-	@PostMapping("/user/create")
+	@PostMapping("")
 	public String create(User user){
 		System.out.println("user : " + user);
 		userRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model){
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
+	}
+	
+	@GetMapping("/form")
+	public String form(){
+		return "/user/form";
+	}
+	
+	@RequestMapping("/login")
+	public String login(){
+		return "/user/login";
 	}
 }
--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,18 +1,15 @@
 package net.slipp.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class WelcomeController {
 	
-	@GetMapping("/helloworld")
-	public String welcome(Model model) {
-		model.addAttribute("name", "chris");
-		model.addAttribute("value", 1000 );
-		model.addAttribute("taxed_value", 1000 - 5);
-		model.addAttribute("in_ca", true);
-		return "welcome";
+	@GetMapping("")
+	public String welcome() {
+		return "index";
 	}
 }
