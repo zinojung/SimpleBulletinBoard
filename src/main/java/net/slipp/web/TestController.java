5bc9af2c3f2f0edfa2f8325722c47014ce7151df
ZINO\46fir <46firstmate@gmail.com>
2017-05-12 21:20:52 +0900

    로그인 기능 구현

---
+++ src/main/java/net/slipp/web/TestController.java
@@ -0,0 +1,17 @@
+package net.slipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("")
+	public String param(){
+		
+		return "test";
+	}
+}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,5 +1,7 @@
 package net.slipp.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,30 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session){
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/login";
+		}
+		if (!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+
+		return "redirect:/";
+	}
+
 	@PostMapping("")
 	public String create(User user){
 		System.out.println("user : " + user);
